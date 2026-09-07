8bc81ca2969b8ed6b8743f4ddcbe42d256be69b1
sovereigndataops-max <sovereigndata.ops@gmail.com>
2026-09-07 00:45:08 -0400

    Update main.py

--- ScamShieldMobile/main.py
+++ ScamShieldMobile/main.py
@@ -1,1 +1,16 @@
+[app]
+title = ScamShield Mobile
+package.name = scamshieldmobile
+package.domain = org.scamshield
+source.include_exts = py,png,jpg,kv
+version = 0.1
 
+requirements = python3,kivy,plyer,requests
+
+orientation = portrait
+
+android.api = 31
+android.minapi = 21
+android.archs = arm64-v8a
+
+fullscreen = 0
